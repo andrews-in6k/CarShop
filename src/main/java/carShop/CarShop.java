de1ba837f7215c9cf7cce2ff2aee6cd9da050431
Andriy Slipenko <andrews@interlink-ua.com>
2015-10-20 17:55:20 +0300

    add base classes

---
+++ src/main/java/carShop/CarShop.java
@@ -0,0 +1,45 @@
+package carShop;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by employee on 10/20/15.
+ */
+public class CarShop {
+    private List<Car> cars = new ArrayList<>();
+    private List<SalesManager> salesManagers = new ArrayList<>();
+    private List<Deal> deals = new ArrayList<>();
+
+    public void addDeal(Deal deal) {
+        deals.add(deal);
+    }
+
+    public void addCar(Car car) {
+        cars.add(car);
+    }
+
+    public void addSalesManager(SalesManager salesManager) {
+        salesManagers.add(salesManager);
+    }
+
+    public void removeCar(Car car) {
+        cars.remove(car);
+    }
+
+    public void removeSalesManager(SalesManager salesManager) {
+        salesManagers.remove(salesManager);
+    }
+
+    public List<Car> getCars() {
+        return cars;
+    }
+
+    public List<SalesManager> getSalesManagers() {
+        return salesManagers;
+    }
+
+    public List<Deal> getDeals() {
+        return deals;
+    }
+}
