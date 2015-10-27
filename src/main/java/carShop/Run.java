472e612ae35e8e5ded1028bc1ea7bd3e49a90d7c
Andriy Slipenko <andrews@interlink-ua.com>
2015-10-27 12:27:42 +0200

    add initConfig.xml

--- src/main/java/carShop/Run.java
+++ src/main/java/carShop/Run.java
@@ -1,9 +1,7 @@
 package carShop;
 
-import carShop.service.CarShopService;
-
-import java.io.PrintStream;
-import java.util.Scanner;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  * Created by employee on 10/20/15.
@@ -11,14 +9,8 @@ import java.util.Scanner;
 public class Run {
 
     public static void main(String[] args) {
-        PrintStream printStream = new PrintStream(System.out);
-        Scanner scanner = new Scanner(System.in);
-
-        CarShopInputStream carShopInputStream = new ConsoleCarShopInputStream(scanner);
-        CarShopPrinter carShopPrinter = new ANSICarShopPrinter(printStream);
-
-        CarShopService carShop = new CarShopService();
-        CarShopController carShopController = new ConsoleCarShopController(carShop, carShopInputStream, carShopPrinter);
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("initConfig.xml");
+        CarShopController carShopController = (CarShopController)applicationContext.getBean("carShopController");
 
         carShopController.startManageCarShop();
     }
