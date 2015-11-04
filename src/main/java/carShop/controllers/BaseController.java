60cc695796af9f216ee045003e7211864b2a2eeb
Andriy Slipenko <andrews@interlink-ua.com>
2015-11-04 06:54:29 +0200

    add cars controller

--- src/main/java/carShop/controllers/BaseController.java
+++ src/main/java/carShop/controllers/BaseController.java
@@ -1,11 +1,14 @@
 package carShop.controllers;
 
+import carShop.core.entity.Car;
 import carShop.service.ServiceInterface;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * Created by employee on 11/3/15.
@@ -23,8 +26,32 @@ public class BaseController {
         return "cars";
     }
 
-    @RequestMapping(value = "/delete", method = RequestMethod.POST)
-    public String deleteCar() {
+    @RequestMapping(value = "/delete/{carId}", method = RequestMethod.GET)
+    public String deleteCar(@PathVariable int carId, ModelMap model) {
+        carShopService.removeCarById(carId);
+
+        model.addAttribute("cars", carShopService.getCars());
+
+        return "cars";
+    }
+
+    @RequestMapping(value = "/add", method = RequestMethod.POST)
+    public String addCar(
+            @RequestParam("textFieldBrand") String brand,
+            @RequestParam("textFieldName") String name,
+            @RequestParam("textFieldCost") String cost,
+            ModelMap model) {
+        if (!brand.equals("") && !name.equals("") && !cost.equals("")) {
+            Car car = new Car();
+
+            car.setBrand(brand);
+            car.setName(name);
+            car.setCost(Integer.parseInt(cost));
+
+            carShopService.addCar(car);
+        }
+
+        model.addAttribute("cars", carShopService.getCars());
 
         return "cars";
     }
