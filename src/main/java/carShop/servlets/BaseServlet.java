26ced50274756d5c0086cf8ae91270f96485b20d
Andriy Slipenko <andrews@interlink-ua.com>
2015-11-04 13:06:24 +0200

    servlets returning

--- src/main/java/carShop/servlets/BaseServlet.java
+++ src/main/java/carShop/servlets/BaseServlet.java
@@ -1,26 +1,26 @@
-//package carShop.servlets;
-//
-//import carShop.service.Service;
-//import carShop.service.ServiceInterface;
-//import org.springframework.web.context.WebApplicationContext;
-//import org.springframework.web.context.support.WebApplicationContextUtils;
-//
-//import javax.servlet.ServletException;
-//import javax.servlet.http.HttpServlet;
-//import javax.servlet.http.HttpServletRequest;
-//import javax.servlet.http.HttpServletResponse;
-//import java.io.IOException;
-//
-///**
-// * Created by employee on 11/2/15.
-// */
-//public class BaseServlet extends HttpServlet{
-//
-//    ServiceInterface carShopService;
-//
-//    @Override
-//    public void init() throws ServletException {
-//        WebApplicationContext ctx = WebApplicationContextUtils.getRequiredWebApplicationContext(getServletContext());
-//        carShopService = (Service) ctx.getBean("service");
-//    }
-//}
+package carShop.servlets;
+
+import carShop.service.Service;
+import carShop.service.ServiceInterface;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by employee on 11/2/15.
+ */
+public class BaseServlet extends HttpServlet{
+
+    ServiceInterface carShopService;
+
+    @Override
+    public void init() throws ServletException {
+        WebApplicationContext ctx = WebApplicationContextUtils.getRequiredWebApplicationContext(getServletContext());
+        carShopService = (Service) ctx.getBean("service");
+    }
+}
